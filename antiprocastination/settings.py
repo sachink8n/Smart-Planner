18252849f0d5d44e6932ee4adfc33a4b38380f93
sachink8n <sachink76339@gmail.com>
2026-03-12 11:06:59 +0530

    Add render.yaml and fix production deployment config

--- antiprocastination/settings.py
+++ antiprocastination/settings.py
@@ -141,6 +141,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+WHITENOISE_MANIFEST_STRICT = False
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
